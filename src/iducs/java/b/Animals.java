67887429e894c855acc5298849bd16d63e2d5aad
maus912 <hun991122@gmail.com>
2019-05-28 17:39:01 +0900

    master revise animal

--- src/iducs/java/b/Animals.java
+++ src/iducs/java/b/Animals.java
@@ -2,7 +2,7 @@ package iducs.java.b;
 
 public interface Animals {
 	String species = "동물"; // public final
-	
+	String common = "동물"; // public final	
 	void move(); // public abstract
 	void eat();
 	void sound();
